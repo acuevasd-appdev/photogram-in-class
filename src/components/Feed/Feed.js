6623d456c32af10898dd55fff947061c9d658078
acuevasd <acuevasd@chicagobooth.edu>
2023-02-28 00:45:17 +0000

    Feed basic, photo word srcroll

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,13 +1,44 @@
 import React from "react";
-import {View,Text} from "react-native";
+import {View,Text, ScrollView} from "react-native";
+import {axiosInstance} from "../../utils";
 
-function Feed(){
+function Feed({user}){
+    const [photos, setPhotos] = React.useState([]);
+
+    async function getFeed() {
+        try {
+            const apiEndPoint = `/${user.username}/feed.json?user_email=${user.email}&user_token=${user.authentication_token}`
+            const response = await axiosInstance.get(apiEndPoint)
+            setPhotos(response.data);
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    React.useEffect(() => {
+        getFeed()
+    },[])
 
     return (
         <View>
             <Text>
-
+                Feed
+                {`/${user.username}/feed.json?user_email=${user.email}&user_token=${user.authentication_token}`}
             </Text>
+            <ScrollView>
+                {
+                    photos.map((photo) => {
+                        return (
+                            <View> 
+                                <Text>
+                                    a photo
+                                </Text>
+                            </View>
+                        )    
+
+                    })
+                }
+            </ScrollView>
         </View>
     )
 }
