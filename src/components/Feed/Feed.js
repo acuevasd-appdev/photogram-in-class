7f59f0a50711c70734f633104f70c61032b63e33
acuevasd <acuevasd@chicagobooth.edu>
2023-02-28 00:22:49 +0000

    building feed

---
+++ src/components/Feed/Feed.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+function Feed(){
+
+    return (
+        <View>
+            <Text>
+
+            </Text>
+        </View>
+    )
+}
+
+export default Feed;
